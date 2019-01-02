3b11a08c72b2da8f42b9c802cd7fbad5bba36c5c
453852802@qq.com <0101xian>
2019-01-02 20:55:24 +0800

    <groupId>在最外层maven中定义就可以了，artifactId与App.java的位置无关

--- provider/src/main/java/demo/App.java
+++ provider/src/main/java/demo/App.java
@@ -1,12 +1,13 @@
-package demo.springeureka;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
+
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
