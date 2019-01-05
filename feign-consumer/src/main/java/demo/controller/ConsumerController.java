77f05f6334112a42a685c02d0788dfe93a78c5e0
453852802@qq.com <0101xian>
2019-01-05 20:54:50 +0800

    feign初探

---
+++ feign-consumer/src/main/java/demo/controller/ConsumerController.java
@@ -0,0 +1,18 @@
+package demo.controller;
+
+import demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping(value = "/ribbon-consumer" ,method = RequestMethod.GET)
+    public String helloConsumer(){
+        return helloService.hello();
+    }
+}
